fa11026a0c1eadd1f7719ee1f44355b17c08333b
Daniel <daniel.ruiz@qaracter.com>
2025-01-15 10:31:17 +0000

    Added 1 variable and constructor

--- Game.java
+++ Game.java
@@ -12,5 +12,16 @@ public class Game {
     private ArrayList<String> enteredLetters;
     private ArrayList<String> foundLetters;
     private ArrayList<String> wrongLetters;
+    private int lives;
+
+    Game(String word_) {
+        word = word_;
+        lettersRemaining = word.length(); // Initialized as the total length of the word
+        letters = new String[lettersRemaining]; // Will contain the letters of the word, each letter will be one element
+        enteredLetters = new ArrayList<String>();
+        foundLetters = new ArrayList<String>();
+        wrongLetters = new ArrayList<String>();
+        lives = 6;
+    }
 }
 
