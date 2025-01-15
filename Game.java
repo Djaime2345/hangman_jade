0f868a45eb9707e3f00776436d9949ef7e0126ba
Daniel <jesus.hernandez@imagina-bpo.es>
2025-01-15 10:28:35 +0000

    Variables Game

--- Game.java
+++ Game.java
@@ -3,5 +3,14 @@ package PACKAGE_NAME;
 // First commit branch game logic
 
 public class Game {
+    private Printer printer = new Printer();
+
+    private String word; // The word to find
+    private int lettersRemaining;
+
+    private String[] letters;
+    private ArrayList<String> enteredLetters;
+    private ArrayList<String> foundLetters;
+    private ArrayList<String> wrongLetters;
 }
 
